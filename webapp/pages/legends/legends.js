51f6e3b69e5cd87763b35d44964c7089b52f60cd
fh <1935889830@qq.com>
2019-04-19 16:00:06 +0800

    手写一个服务器

---
+++ webapp/pages/legends/legends.js
@@ -0,0 +1,18 @@
+Page({
+  data: {
+    legends: [
+      {
+        name: '阿卡丽',
+        title: '刺客'
+      },
+      {
+        name: '亚索',
+        title: '风男'
+      },
+      {
+        name: '诺克',
+        title: '坦克'
+      }
+    ]
+  }
+})
