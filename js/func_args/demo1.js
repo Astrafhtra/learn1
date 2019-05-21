93ed461809196ab52da4af3403dfdac1c1b911cb
fh <1935889830@qq.com>
2019-05-21 21:33:56 +0800

    异步请求

---
+++ js/func_args/demo1.js
@@ -0,0 +1,6 @@
+function information(name,sex,money,birthday,color,age){
+  return `${name} ${sex} ${money} ${birthday} ${color} ${age}`
+}
+let user = {name:'松涛',sex:'男',money:'十个亿',birthday:'5.20',color:'yellow',age:'18'};
+// let name = name
+information(user);
---
+++ js/func_args/demo2.js
@@ -0,0 +1,5 @@
+function information({name,sex,money,birthday,height,age}){
+  console.log(name,sex,money,birthday,height,age);
+}
+let user = {name:'付豪',sex:'男',money:'十个亿',birthday:'5.20',height:170,age:'18'};
+information(user)
